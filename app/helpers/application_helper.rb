e2b7e112ad52a09d6a3a2c69038f08279261d9dc
hibariya <celluloid.key@gmail.com>
2010-08-12 22:48:03 +0900

    hogera

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def string_limit(s, len=20, ovrflw='...')
-   s.scan(/./)[0..len] + (s.length>len ? ovrflw: '')
+   s.scan(/./)[0..len].join + (s.length>len ? ovrflw: '')
   end
 end
