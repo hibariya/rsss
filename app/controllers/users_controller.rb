197e5878524fe6952bb401a08375bb1de745d0b1
hibariya <celluloid.key@gmail.com>
2010-08-14 18:26:28 +0900

    属性消し忘れ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   before_filter ApplicationController.check_admin
   
   def update
-    @user.name = params[:user][:name]
     @user.site = params[:user][:site]
     @user.description = params[:user][:description]
     if @user.valid?
