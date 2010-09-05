402a98914ef8480e1ac1f248bbe55f5066fa9930
hibariya <celluloid.key@gmail.com>
2010-09-05 14:53:36 +0900

    secret_tokenを環境変数にもっていった

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rsss::Application.config.secret_token = 'e59a7f0700b4b6f3aac492952ebecf27ea2bfe4f585f860032ebf6cb8e19cbf5ed0bc451d81fbd794ba3df9634c4fc2fc46437ee6d937f186cc588d94973c247'
+Rsss::Application.config.secret_token = ENV['SECRET_TOKEN']
+
