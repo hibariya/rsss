7b71c37ddd4d4a8641a8a3c86387f88c95bd74e0
hibariya <celluloid.key@gmail.com>
2010-09-26 14:17:44 +0900

    RSS::Makerにnilを渡すとうまくいかないっぽい

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -25,18 +25,18 @@ class IndexController < ApplicationController
     RSS::Maker.make('1.0') do |maker|
       maker.channel.about = URI.join("http://rsss.be", user_feed_path(@focus_user.screen_name))
       maker.channel.title = ['RSSS | ', @focus_user.screen_name].join
-      maker.channel.description = @focus_user.description
+      maker.channel.description =  @focus_user.description || ''
       maker.channel.link = URI.join("http://rsss.be", user_page_path(@focus_user.screen_name))
       maker.channel.author = @focus_user.screen_name
-      maker.channel.date = @focus_user.updated_at
+      maker.channel.date = @focus_user.updated_at || Time.now
       maker.items.do_sort = true
 
       @focus_user.recent_entries.each do |entry|
         maker.items.new_item do |item|
-          item.link = entry.link.toutf8
-          item.title = entry.title.toutf8
-          item.date = entry.date
-          item.description = entry.content.toutf8
+          item.link = entry.link || ''
+          item.title = entry.title || ''
+          item.date = entry.date || Time.now # TODO: 今の時刻なわけがない
+          item.description = entry.content || ''
         end
       end
     end
