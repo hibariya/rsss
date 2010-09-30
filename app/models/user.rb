03026e8953b3a68ef2eca323bc8cf269039f66d5
hibariya <celluloid.key@gmail.com>
2010-10-01 00:32:48 +0900

    タグ(カテゴリ)にドットの含まれているものは今のところ対応できないので除外

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,10 +49,14 @@ class User
     where(:screen_name=>s)
   }
 
+  #
+  # ドット(.)の含まれていないすべてのカテゴリをサマる
+  #
   def categories_summary
     @categories_summary ||= Rsss::Summarize.
       segment(sites.map(&:categories).flatten.
-        map(&:downcase).inject({}){|r,c| r[c]||=0; r[c]+=1; r}.to_a)
+        reject{|c| c =~ /\./ }.map(&:downcase).
+        inject({}){|r,c| r[c]||=0; r[c]+=1; r}.to_a)
   end
   
   def recent_entries
