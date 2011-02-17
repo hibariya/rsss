3bc3041871c4e09dfd1853dcd0d9ab57656e468c
hibariya <celluloid.key@gmail.com>
2011-02-17 10:01:34 +0900

    application error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-  layout 'application'
 
-  #rescue_from :with => :application_error
+  protect_from_forgery
+
+  before_filter :require_user
+
+  rescue_from Exception, :with => :application_error if %w(production).include? Rails.env
+
+  layout 'application'
 
   def current_user
     @current_user ||= User.where(:token => session[:user_token]).first
@@ -20,21 +24,50 @@ class ApplicationController < ActionController::Base
   end
 
   def require_user
-    unless signed_in?
-      render :controller => :sessions,
-             :action     => :failure,
-             :status     => 403
-      return false
-    end
+    raise Rsss::NotAuthorized unless signed_in?
   end
 
   private
   
     def application_error(e)
       ErrorLog.add e
+      flash.now[:error] = error_message(e)
       respond_to do |format|
-        format.html{ render :controller => :index, :action => :failure }
-        format.xml{ render :controller => :index, :action => :failure, :layout => false }
+        format.html do
+          render :layout => false, :template => 'error',
+                 :status => status_code(e)
+        end
+
+        format.xml do
+          render :layout => false, :text => '', :status => status_code(e)
+        end
       end
     end
+
+    def status_code(e)
+      if [ActionController::RoutingError, Rsss::UserNotFoundError].include? e.class
+        404
+
+      elsif [Rsss::NotAuthorized].include? e.class
+        403
+
+      else 
+        500
+
+      end
+    end
+
+    def error_message(e)
+      if [ActionController::RoutingError, Rsss::UserNotFoundError].include? e.class
+        'ユーザまたはページが見つかりませんでした。'
+
+      elsif [Rsss::NotAuthorized].include? e.class
+        'サインインが必要です'
+
+      else 
+        '未分類の内部エラー'
+
+      end
+    end
+
 end
---
+++ config/initializers/exceptions.rb
@@ -0,0 +1,6 @@
+module Rsss
+  class UserNotFoundError < StandardError; end
+  class FeedOpenError < StandardError; end
+  class NotAuthorized < StandardError; end
+
+end
