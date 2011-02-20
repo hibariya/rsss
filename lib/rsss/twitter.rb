c0b7a633996098244697ec659e96ff6ce8840e8f
hibariya <celluloid.key@gmail.com>
2011-02-21 01:05:41 +0900

    fix typo

--- lib/rsss/twitter.rb
+++ lib/rsss/twitter.rb
@@ -5,14 +5,18 @@ module Rsss
     class << self
       def consumer
         @consumer ||= ::OAuth::Consumer.new(
-          Rsss::OAUTH_CONSUER_KEY,
+          Rsss::OAUTH_CONSUMER_KEY,
           Rsss::OAUTH_CONSUMER_SECRET,
           {:site=>'https://twitter.com'})
       end
 
       def user_info(token, secret)
+        ::JSON.parse access_token(token, secret). 
+          get('/account/verify_credentials.json').body
+      end
+
+      def access_token(token, secret)
         access_token = ::OAuth::AccessToken.new consumer, token, secret
-        ::JSON.parse access_token.get('/account/verify_credentials.json').body
       end
 
       def request_token(request_token, request_token_secret)
